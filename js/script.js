d4717215b56fa52e5733f448bbd7fb4785ccb6f8
Njoki <kinuthiash@gmail.com>
2022-03-19 19:57:10 +0300

    feat:script-add form validation

--- js/script.js
+++ js/script.js
@@ -2,17 +2,42 @@
 // let body = document.getElementById("body");
 //  body.style.backgroundColor ="red";
 
+
+// BUSINESS LOGIC CODE
+// Validation of form function
+let validate = function(size, crust, topping,quantity,delivery){
+  if (!size || !crust || !topping || !quantity ||!delivery) return false;
+  return true
+}
+
+
+// clear form code function
+
+// USER INTERFACE CODE
 $(document).ready(function() { 
   $(".submit").submit(function(event){
     event.preventDefault();
+    // get user input
     let size = $("input:radio[name=size]:checked").val();
     let crust = $("input:radio[name=crust]:checked").val();
-    let topping = $("input:radio[name=topping]:checked").val()
-    
+    let topping = $("input:radio[name=topping]:checked").val()    
     let quantity = $("input#quantity").val();
     let delivery = $("input:radio[name=delivery]:checked").val();
 
-    alert(quantity)
+    // validate form
+    if(validate(size,crust,topping,quantity,delivery)){
+      alert("Order received")
+      if(delivery==="deliver"){
+        prompt("Please insert drop off location")
+      }
+      else{
+        alert("okay")
+      }
+    }
+    else{
+      alert("Please fill out out all the choices")     
+    }     
   })
 })
+
 
